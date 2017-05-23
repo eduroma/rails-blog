565d4de347fb4d203f7a1ddf2b1e2469bd0c000f
Eduardo C. Romanini <eduroma@globo.com>
2017-05-23 19:25:14 -0300

    Tópico 5.10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,45 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article=Article.new
+  end
+
+  def edit
   end
 
   def create
 
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      # A opção pelo "render" e não "redirect_to" é porque o redirect_to iria realizar uma nova requisição da
+      #varicavel @article. Já o render, mantem a mesma requisição do objeto.
+      render 'new'
+    end
+
       #Gera um "hash" com os parametros recebidos do formulário article.
-      render plain: params[:article].inspect
+      #render plain: params[:article].inspect
+      #Hash gerada:
+      #<ActionController::Parameters {"title"=>"First Article!", "text"=>"This is my first article."} permitted: false>
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
